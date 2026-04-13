b513a80e658dd960a6004bc847d6a0965e9b1135
etud user <etud@localhost>
2026-04-13 09:57:58 +0200

    feat: S3.3 lecteur vidéo frame précis

--- frontend/src/components/video/VideoPlayer.tsx
+++ frontend/src/components/video/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useImperativeHandle, forwardRef } from 'react'
+import { useRef, useEffect, useImperativeHandle, forwardRef } from 'react'
 import { useVideoStore } from '../../stores/videoStore'
 import { useRequestVideoFrame } from '../../hooks/useRequestVideoFrame'
 import { seekToFrame } from '../../hooks/useFrameSeek'
